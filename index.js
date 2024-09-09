6ab5f74f682a05d666662c162188a8c828b4c23e
Yash Kumar Shrivas <yashkshrivas1234@gmail.com>
2024-09-09 14:50:22 +0530

    Chore : Initial config ✅

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const PORT = 8080;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server 👨‍💻");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  socket.on("join-room", (room, username) => {
+    socket.join(room);
+    console.log(`User ${username || socket.id} joined room ${room}`);
+
+    io.to(room).emit(
+      "userJoined",
+      `${username || socket.id} has joined the room`
+    );
+  });
+
+  socket.on("message", ({ room, message }) => {
+    console.log(`Message in ${room}: ${message}`);
+    io.to(room).emit("receive-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
